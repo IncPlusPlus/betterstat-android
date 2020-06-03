9c8ca03bf07eafde7a7423e641818c87ca36eaca
Ryan Cloherty <cloherty.ryan@gmail.com>
2020-06-03 17:50:08 -0400

    Remove the timeout for now
    The message it provides to the user is really unhelpful. The socket exception that shows up when the connection is refused is more helpful.

--- lib/api/schedule/api_schedule.dart
+++ lib/api/schedule/api_schedule.dart
@@ -17,14 +17,14 @@ Future<Schedule> postSchedule(Schedule schedule) async {
   final uri = Uri.http(apiUrl, '$scheduleEndpoint');
   final response = await httpClient.put(uri,
       headers: applicationJsonHeader,
-      body: jsonEncode(serialize<Schedule>(schedule))).timeout(timeout);
+      body: jsonEncode(serialize<Schedule>(schedule)));
   _expectResponseCode(201, response);
   return deserialize<Schedule>(jsonDecode(response.body));
 }
 
 Future<List<Schedule>> getSchedules() async {
   final uri = Uri.http(apiUrl, scheduleEndpoint);
-  final response = await httpClient.get(uri).timeout(timeout);
+  final response = await httpClient.get(uri);
   _expectResponseCode(200, response);
   // Use the compute function to run parseSchedules in a separate isolate.
   return compute(_parseSchedules, response.body);
@@ -32,7 +32,7 @@ Future<List<Schedule>> getSchedules() async {
 
 Future<Schedule> getSchedule(String id) async {
   final uri = Uri.http(apiUrl, '$scheduleEndpoint/$id');
-  final response = await httpClient.get(uri).timeout(timeout);
+  final response = await httpClient.get(uri);
   _expectResponseCode(200, response);
   return deserialize<Schedule>(jsonDecode(response.body));
 }
@@ -41,14 +41,14 @@ Future<Schedule> putSchedule(Schedule schedule, String id) async {
   final uri = Uri.http(apiUrl, '$scheduleEndpoint/$id');
   final response = await httpClient.put(uri,
       headers: applicationJsonHeader,
-      body: jsonEncode(serialize<Schedule>(schedule))).timeout(timeout);
+      body: jsonEncode(serialize<Schedule>(schedule)));
   _expectResponseCode(200, response);
   return deserialize<Schedule>(jsonDecode(response.body));
 }
 
 Future<Schedule> deleteSchedule(Schedule schedule, String id) async {
   final uri = Uri.http(apiUrl, '$scheduleEndpoint/$id');
-  final response = await httpClient.delete(uri).timeout(timeout);
+  final response = await httpClient.delete(uri);
   _expectResponseCode(200, response);
   return deserialize<Schedule>(jsonDecode(response.body));
 }
